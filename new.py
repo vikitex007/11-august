aaf2667a7df79735c622c4f7b1748656064ac8c6
sachin shrestha <sachecka1176@gmail.com>
2022-08-11 11:42:21 +0545

    my first commit

---
+++ new.py
@@ -0,0 +1,15 @@
+# ---> It represent a one line comment in python 
+
+from email.headerregistry import HeaderRegistry
+
+
+'''
+
+It  represent a multi-line comment 
+We can write any thing HeaderRegistry
+'''
+
+a = 5 #there, is a variable which stores/hold integer type value i.e 5 
+nome  = 'Sachin' where, name ina variable which stores/hold string type value i.e "Sachin"
+
+# And in python string are always inside single orn double 
